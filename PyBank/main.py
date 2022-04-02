e94d02e77fd5fd65279c7440cf46d6340d375963
Aline Hornoff <australine@gmail.com>
2022-04-02 10:57:47 +0800

    Added new code to PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,23 +3,34 @@ import csv
 
 budget_data_csv = os.path.join("Resources", "budget_data.csv")
 
+def budget_data(month_data):
+    date = str(month_data[0])
+    profit = int(month_data[1])
+
 # Open and read csv
-with open(budget_data_csv) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=",")
+with open(budget_data_csv, 'r') as csv_file:
 
-    # Read the header row first 
-    csv_header = next(csv_file)
-    print(f"Header: {csv_header}")
 
-    # Read each row of data after the header
-    for row in csv_reader:
-        print(row)
+    # Read the header and skip the header
+    csvreader = csv.reader(csv_file, delimiter=',')
+    csv_reader = next(csvreader)
 
     # Add text 'Financial Analysis'
+    print("Financial Analysis")
+
     # Add '------' break
+    print("----------------------")
+
     # Create total number of month included in the data set
+    month_count = sum(1 for lin in csv_file)
+    print(f"Total Months: {month_count}")
+
     # Create net total amount of "Profit/Losses" over the entire period
+    NetProfitLoss = sum(int(NetProfitLoss[1]))
+    print(f"Total: {NetProfitLoss}")
+
     # Find the greatest increase in profts (date and amount) over the entire period
+    # Find the average of the change in "Profit/Losses" the entire period
     # Find the greatest decrease in loses (date and amount) over the entire period
     # Summarise and print 'Financial Analysis'
     # Export a text file with the results    
