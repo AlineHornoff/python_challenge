65c7a3ee4f85faf481fecd88956b0686a6875a50
Aline Hornoff <australine@gmail.com>
2022-04-02 13:22:23 +0800

    Added new code for profit and loss to PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,36 +3,79 @@ import csv
 
 budget_data_csv = os.path.join("Resources", "budget_data.csv")
 
-def budget_data(month_data):
-    date = str(month_data[0])
-    profit = int(month_data[1])
+#def budget_data(month_data):
+    #month = str(month_data[0])
+    #profit_loss = int(month_data[1])
+    #return profit_loss
 
 # Open and read csv
 with open(budget_data_csv, 'r') as csv_file:
 
 
     # Read the header and skip the header
-    csvreader = csv.reader(csv_file, delimiter=',')
+    csvreader = csv.reader(csv_file)
     csv_reader = next(csvreader)
 
-    # Add text 'Financial Analysis'
-    print("Financial Analysis")
-
-    # Add '------' break
-    print("----------------------")
-
     # Create total number of month included in the data set
-    month_count = sum(1 for lin in csv_file)
-    print(f"Total Months: {month_count}")
+    month_count = sum(1 for row in csv_file)
 
+    # reset csvreader
+    csv_file.seek(0)
+    next(csvreader)
+    
     # Create net total amount of "Profit/Losses" over the entire period
-    NetProfitLoss = sum(int(NetProfitLoss[1]))
-    print(f"Total: {NetProfitLoss}")
+    sum_profit = 0
+    sum_loss = 0
+    TotalProfitLoss = 0
+    Profit = 0
+    
+    for row in csvreader:
+        profit = int(row[1])
+        if profit > 0:
+            sum_profit = sum_profit + profit
+        elif profit < 0:
+            sum_loss = sum_loss + profit
+    TotalProfitLoss = sum_profit - sum_loss
 
-    # Find the greatest increase in profts (date and amount) over the entire period
-    # Find the average of the change in "Profit/Losses" the entire period
+    # reset csvreader
+    csv_file.seek(0)
+    next(csvreader)
+
+    # Find the average of the change in "Profit/Losses" for the entire period
+    total = []
+
+    for row in csvreader:
+        AverageChange = [y-x for x, y in zip(total[:-1], total[1:])]
+ 
+    # reset csvreader
+    csv_file.seek(0)
+    next(csvreader) 
+
     # Find the greatest decrease in loses (date and amount) over the entire period
+    #GreatestIncrease = []
+    #GreatestDecrease = []
+    #months = []
+
+    # Look for index number of greatest increase and store in 'MonthsMaxIndex'
+    #for a, b in enumerate(GreatestIncrease):
+        #if b == min(GreatestIncrease):
+            #MonthsMaxIndex = (a)
+
+    # Store the month with the greatest increase in 'GreatestIncreaseMonth'
+    #for c in [months[MonthsMaxIndex]]:
+        #GreatestIncreaseMonth = c
+
+    
+
     # Summarise and print 'Financial Analysis'
+    print("Financial Analysis")
+    print("----------------------")
+    print(f"Total Months: {month_count}")
+    print(f"Total: ${TotalProfitLoss}")
+    print(f"Average Change: ${round(sum(AverageChange)/len(AverageChange),2)}")
+    print(f"Greatest Increase in Profits: {GreatestIncreaseMonth} (${max(GreatestIncrease)}")
+    print(f"Greatest Decrease in Profits: {month}")
+
     # Export a text file with the results    
 
     
